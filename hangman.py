bf0cf5e6b6223d294888c303b399ac6e6bfc9564
Refun <refunfk@gmail.com>
2022-08-05 22:59:57 +0530

    GREEN: 12 TEST IS PASSED

--- hangman.py
+++ hangman.py
@@ -10,7 +10,7 @@ def get_random_word(path):
         word=word.strip()
         if  not word.isalpha():
             continue
-        if len(word) < 7:
+        if len(word) < 6:
             continue
         if word[0].isupper():
             continue
@@ -18,7 +18,7 @@ def get_random_word(path):
 
     return random.choice(rand_word)
     
-def find_sentence (word,guess_char):
+def hidden_word (word,guess_char):
     hiden_word= ''
     for letter in word: 
         if letter in guess_char:
@@ -26,3 +26,45 @@ def find_sentence (word,guess_char):
         else:
             hiden_word += '-'
     return hiden_word
+
+
+def get_status(secret_word, guesses, turns_left):
+    masked_word = hidden_word(secret_word, guesses)
+    return f"""Word : {masked_word}
+Turns left : {turns_left}"""
+
+def evaluate_input(secret_word, guesses, turns_left, input_):
+    if input_ in guesses:
+        return f"You already guessed '{input_}'", turns_left
+    
+    if "-" not in hidden_word(secret_word, guesses+[input_]):
+        return "word has been guessed", turns_left
+
+    if input_ in secret_word:
+        guesses.append(input_)
+        return "good guess", turns_left
+
+    if input_ not in secret_word:
+        if turns_left == 1:
+            return "game is over", turns_left
+        else:
+            guesses.append(input_)
+            return "bad guess", turns_left - 1
+
+def hangman():
+    secret_word = get_random_word(path)
+    print (secret_word)
+    guesses = []
+    turns_left = 7
+    status = ""
+    while status not in ["word has been guessed", "game is over"]:
+        print ("\n", status)
+        print (get_status(secret_word, guesses, turns_left))
+        guess = input("Enter a letter ")
+        print (f'"{guess}"')
+        status, turns_left = evaluate_input(secret_word, guesses, turns_left, guess)
+
+if __name__ == "__main__":
+    hangman()
+
+        
--- test.py
+++ test.py
@@ -43,13 +43,71 @@ def test_get_random_word():
 
 def test_get_secret_word():
     my_word ='elephant'
-    assert hangman.find_sentence(my_word,[]) == '--------'
-    assert hangman.find_sentence(my_word,['e']) == 'e-e-----'
-    assert hangman.find_sentence(my_word,['e','a']) == 'e-e--a--'
-    assert hangman.find_sentence(my_word,['e','a','x']) == 'e-e--a--'
+    assert hangman.hidden_word(my_word,[]) == '--------'
+    assert hangman.hidden_word(my_word,['e']) == 'e-e-----'
+    assert hangman.hidden_word(my_word,['e','a']) == 'e-e--a--'
+    assert hangman.hidden_word(my_word,['e','a','x']) == 'e-e--a--'
 
 def test_mark_word_invalid_guesses():
     for i in ["police", "elephant", "it", "foo"]:
-        assert hangman.find_sentence(i, ["x", "q"]) == len(i)*"-"
+        assert hangman.hidden_word(i, ["x", "q"]) == len(i)*"-"
+
+
+def test_get_status():
+    secret_word = "aeroplane"
+    guesses = ['x', 't', 'p', 'a']
+    status = hangman.get_status(secret_word, guesses, 5)
+    assert status == f"""Word : a---p-a--
+Turns left : 5"""
+
+
+def test_evaluate_input_word_guessed():
+    secret_word = "cat"
+    guesses = ["c", "a", "x", "m"]
+    turns_left = 5
+    input_ = "t"
+    resp, turns_left = hangman.evaluate_input(secret_word, guesses, turns_left, input_)
+    assert turns_left == 5
+    assert resp == "word has been guessed"
+    
+def test_evaluate_input_bad_guess():
+    secret_word = "cat"
+    guesses = ["c", "a", "x", "m"]
+    turns_left = 5
+    input_ = "q"
+    resp, turns_left = hangman.evaluate_input(secret_word, guesses, turns_left, input_)
+    assert resp == "bad guess"
+    assert guesses == ["c", "a", "x", "m", "q"]
+    assert turns_left == 4
+
+    
+def test_evaluate_input_game_over():
+    secret_word = "cat"
+    guesses = ["c", "a", "x", "m"]
+    turns_left = 1
+    input_ = "q"
+    resp, _ = hangman.evaluate_input(secret_word, guesses, turns_left, input_)
+    assert resp == "game is over"
+
+def test_evaluate_input_repeat_guess():
+    secret_word = "cat"
+    guesses = ["c", "a", "x", "m"]
+    turns_left = 5
+    input_ = "c"
+    resp, turns_left = hangman.evaluate_input(secret_word, guesses, turns_left, input_)
+    assert turns_left == 5
+    assert resp == "You already guessed 'c'"
+
+def test_evaluate_input_good_guess():
+    secret_word = "cat"
+    guesses = ["c", "x", "m"]
+    turns_left = 5
+    input_ = "a"
+    resp, turns_left = hangman.evaluate_input(secret_word, guesses, turns_left, input_)
+    assert resp == "good guess"
+    assert turns_left == 5
+
+
+    
 
 
